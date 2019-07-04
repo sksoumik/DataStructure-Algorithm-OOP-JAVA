39eca874e3af9762dd6c8d8e8ce03b1e923cac94
sksoumik <sadmanks@gmail.com>
2019-07-04 19:02:00 +0600

    Interface example 1

--- interfaces/Animal.java
+++ interfaces/Animal.java
@@ -6,17 +6,19 @@ interface Animal{
     public void animalSound();
     public void sleep();
 }
+
+
 class Pig implements Animal {
+
     public void animalSound(){
         System.out.println("The pig says: wee wee");
     }
+
     public void sleep(){
         System.out.println("ZZzz");
-
     }
-
 }
-// Driver class
+/********************* Driver Program ************************/
 class Test{
     public static void main(String[] args) {
         Pig myPig = new Pig();
